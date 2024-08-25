c04ac105349646e54ac54e64cf87e1256cd9598b
Tejomay Gadgil <tejomaygadgil@gmail.com>
2024-08-24 18:16:55 -0700

    Add SICP 1.2

---
+++ 1.15.scm
@@ -0,0 +1,15 @@
+(define (sine-steps angle)
+  (define steps 0)
+  (define (cube x) (* x x x))
+  (define (p x)
+    (set! steps (+ 1 steps))
+    (- (* 3 x) (* 4 (cube x))))
+  (define (sine angle)
+    (if (not (> (abs angle) 0.1))
+      angle
+      (p (sine (/ angle 3.0)))))
+  (sine angle)
+  steps)
+
+(display (sine-steps 12.15))
+(newline)
---
+++ 1.18.scm
@@ -0,0 +1,15 @@
+;;;; 1.18
+;; Using the results of exercises 1.16 and 1.17, devise a procedure that generates an iterative process for multiplying two integers in terms of adding, doubling, and halving and uses a logarithmic number of steps.
+
+;;; Answer
+(load "0.util.scm")
+(define (iter-mult a b)
+  (define (double x) (+ x x))
+  (define (halve x) (/ x 2))
+  (define (iter a b o)
+    (cond ((= b 0) o)
+          ((even? b) (iter (double a) (halve b) o))
+          (else (iter a (- b 1) (+ o a)))))
+  (iter a b 0))
+
+(test (iter-mult 5 17) (* 5 17))
